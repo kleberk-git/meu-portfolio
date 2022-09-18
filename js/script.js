0d1c6890f5c3b82fb3d7e6529005d9b6619cacf5
Kleber Belo <73143439+kleberk-git@users.noreply.github.com>
2022-09-18 10:21:24 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,47 @@
 
+function mostrar_html() {
+
+   var texto_curto = document.getElementById('texto-curto').innerHTML="HTML é uma linguagem de marcação utilizada na construção de páginas na Web.";
+}
+function mostrar_css() {
+    var texto_curto = document.getElementById('texto-curto').innerHTML="CSS Cascading Style Sheets é um mecanismo para adicionar estilo a um documento web.";
+ }
+  
+ function mostrar_js() {
+    var texto_curto = document.getElementById('texto-curto').innerHTML="JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma.";
+ }
+  
+ function mostrar_php() {
+    var texto_curto = document.getElementById('texto-curto').innerHTML="PHP é uma linguagem interpretada livre, usada originalmente apenas para o desenvolvimento de aplicações no lado do servidor, capazes de gerar conteúdo dinâmico Web. ";
+ }
+
+ function mostrar_mysql() {
+    var texto_curto = document.getElementById('texto-curto').innerHTML="O MySQL é um sistema de gerenciamento de banco de dados, que utiliza a linguagem SQL como interface.";
+ }
+
+ function mostrar_sql() {
+    var texto_curto = document.getElementById('texto-curto').innerHTML="Structured Query Language, ou Linguagem de Consulta Estruturada ou SQL, é a linguagem de pesquisa declarativa padrão para banco de dados relacional.";
+ }
+
+ function mostrar_python() {
+    var texto_curto = document.getElementById('texto-curto').innerHTML="Python é uma linguagem de programação de alto nível, interpretada de script, imperativa, orientada a objetos, funcional, de tipagem dinâmica e forte.";
+ }
+
+ function mostrar_c() {
+    var texto_curto = document.getElementById('texto-curto').innerHTML="C é uma linguagem de programação compilada de propósito geral, estruturada, imperativa, procedural.";
+ }
+
+ function mostrar_ccc() {
+    var texto_curto = document.getElementById('texto-curto').innerHTML="C++ é uma linguagem de programação compilada multi-paradigma e de uso geral.";
+ }
+
+ function mostrar_delphi() {
+    var texto_curto = document.getElementById('texto-curto').innerHTML="Embarcadero Delphi, anteriormente conhecido como CodeGear Delphi, Inprise Delphi e Borland Delphi, também conhecido como Delphi.";
+ }
+  
+
+  
+  
+  
+  
+  
